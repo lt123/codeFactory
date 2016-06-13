2acfbac54bba00287c0bc399f0ff367a9609a9e4
lt123 <lolzqwz@foxmail.com>
2016-06-13 17:02:57 +0800

    Update CodeFactoryTest.java

--- src/test/java/com/app/code/test/CodeFactoryTest.java
+++ src/test/java/com/app/code/test/CodeFactoryTest.java
@@ -4,6 +4,12 @@ import com.app.code.util.CreateUtil;
 
 public class CodeFactoryTest {
 	public static void main(String[] args) {
+		// 默认只生成 model dao service serviceImpl mapper
 		CreateUtil.createCode("user", true);
+		
+		// 如果想自己控制生成的文件 可通过Fileflag来设置
+		/*FlagModel flagModel = new FlagModel();
+		flagModel.setCreateModel(true);
+		CreateUtil.createCode("user", true, flagModel);*/
 	}
 }
